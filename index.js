ba3e67f2a151960ccbc9d2695e8497a8453315fe
Bl00D4NGEL <kuhlesdominik@gmx.de>
2019-11-27 17:05:14 +0100

    moved exports into index file

---
+++ index.js
@@ -0,0 +1,27 @@
+import Label from "./Label/Label";
+import Button from "./Button/Button";
+import Checkbox from "./Checkbox/Checkbox";
+import CustomForm from "./Form/Form";
+import Loader from "./Loader/Loader";
+import FakeLoader from "./Loader/FakeLoader";
+import PasswordInput from "./PasswordInput/PasswordInput";
+import RadioButton from "./RadioButton/RadioButton";
+import Select from "./Select/Select";
+import SubmitButton from "./SubmitButton/SubmitButton";
+import Table from "./Table/Table";
+import TextInput from "./TextInput/TextInput";
+
+export {
+    Button,
+    Checkbox,
+    CustomForm,
+    Label,
+    Loader,
+    FakeLoader,
+    PasswordInput,
+    RadioButton,
+    Select,
+    SubmitButton,
+    Table,
+    TextInput,
+};
