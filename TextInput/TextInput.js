76151c05b6d00d9029c92c3fa3e5646aca322f3a
Bl00D4NGEL <kuhlesdominik@gmx.de>
2019-11-27 18:41:59 +0100

    .

--- TextInput/TextInput.js
+++ TextInput/TextInput.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-export default function TextInput({onChange, onChangeSetter, ...other}) {
-    if (onChangeSetter !== undefined) {
-        onChange = (e) => onChangeSetter(e.target.value);
-    }
-    return <input {...other} type="text" onChange={onChange}/>
+export default function TextInput(props) {
+    return <input {...props} type="text" onChange={props.onChange}/>
 }
